6543f1361b47bc37782282f0aa35dc0a3ded46eb
lvuletic <luka.vuletic1999@gmail.com>
2021-05-10 19:55:12 +0200

    centralize .env

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: '../../.env' });
 
 ReactDOM.render(
   <React.StrictMode>
